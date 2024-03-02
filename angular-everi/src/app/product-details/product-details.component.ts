bd8ec0fbabb38b5d40f91b491fe351990382fa0b
Hannie Dong <hannie@Hannies-MacBook-Pro.local>
2024-03-01 21:55:58 -0800

    Add routes

--- angular-everi/src/app/product-details/product-details.component.ts
+++ angular-everi/src/app/product-details/product-details.component.ts
@@ -5,7 +5,7 @@ import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css'],
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
   productId!: number;
